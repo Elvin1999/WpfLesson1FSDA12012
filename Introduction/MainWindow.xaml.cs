e305ec46e4e03ae7fec436b892ce2bda3f67fd40
Elvin <elvincamalzade00@gmail.com>
2021-06-25 16:48:47 +0400

    Add project files.

---
+++ Introduction/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Introduction
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mybtn_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Hi Wpf","C# Language", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.OK)
+            {
+                MyGrid.Background = Brushes.LightCoral;
+            }
+        }
+        int clickCount = 0;
+        private void mybtn2_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ++clickCount;
+            //mybtn2.Content = clickCount;
+           // mystackpanel.Orientation = Orientation.Horizontal;
+        }
+
+        private void mybtn2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void mybtn2_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
